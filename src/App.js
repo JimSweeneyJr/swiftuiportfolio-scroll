3677a3db032227faba221d30250ca7ef62d59f1d
JimSweeneyJr <jimsweeney566@gmail.com>
2023-08-11 14:41:37 -0400

    Push Test Only

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is only a test for GitHub
         </a>
       </header>
     </div>
